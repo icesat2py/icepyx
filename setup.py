3b178a3cb36afda73e6338cfbbd317d06e195083
JessicaS11 <jessica.scheick@maine.edu>
2019-08-22 17:13:50 -0400

    add setup and __init__ files to repo; populate setup file with basic info

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PIEintheSCY",
+    version="0.0.1",
+    author="Jessica Scheick",
+    author_email="jbscheick@gmail.com",
+    description="Processing ICESat-2 Elevations...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ICESAT-2HackWeek/icesat2py.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        
+    ],
+)

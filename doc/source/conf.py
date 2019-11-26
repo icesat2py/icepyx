b92c5cb00954cdd24d3bd6192e2591622bb1054c
JessicaS11 <jessica.scheick@maine.edu>
2019-11-26 10:54:01 -0500

    added files to build docs with sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,7 +27,9 @@ author = 'Jessica Scheick, Anthony Arendt, Lindsey Heagy, Fernando Perez, Amy St
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = [
+    "sphinx.ext.autodoc",
+    "numpydoc"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +38,11 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['**.ipynb_checkpoints']
+
+
+# -- Configuration options ---------------------------------------------------
+autosummary_generate = True
 
 
 # -- Options for HTML output -------------------------------------------------

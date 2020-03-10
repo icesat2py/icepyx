d29a3ad8ec58240aa251155b2861306668dd27b2
JessicaS11 <jessica.scheick@maine.edu>
2020-03-10 18:35:29 +0000

    add stages and jobs to travis

--- icepyx/tests/test_is2class_NSIDC-API.py
+++ icepyx/tests/test_is2class_NSIDC-API.py
@@ -6,4 +6,9 @@ import warnings
 #check that agent key is added in event of no subsetting
 #check that downloaded data is subset
 
-#def test_earthdata_login():
+def test_earthdata_login():
+    #fake test just so there's an active test in this file
+    lcds = 'atl03'
+    obs = ipd._validate_dataset(lcds)
+    expected = 'ATL03'
+    assert obs == expected

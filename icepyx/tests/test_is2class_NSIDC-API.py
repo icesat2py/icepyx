34be32a3bbe374614023927aa59f3d87e29083a3
JessicaS11 <jessica.scheick@maine.edu>
2020-03-12 17:57:35 +0000

    use fixtures for region and session. nsidc_tests

--- icepyx/tests/test_is2class_NSIDC-API.py
+++ icepyx/tests/test_is2class_NSIDC-API.py
@@ -10,15 +10,20 @@ import getpass
 #check that agent key is added in event of no subsetting
 #check that downloaded data is subset
 
+@pytest.fixture
+def reg_a():
+    return ipd.Icesat2Data('ATL06',[-64, 66, -55, 72],['2019-02-22','2019-02-28'])
+
 @patch("getpass.getpass")
-def setup_function():
-    reg_a = ipd.Icesat2Data('ATL06',[-64, 66, -55, 72],['2019-02-22','2019-02-28'])
-    session = reg_a.earthdata_login('icepyx_devteam', 'icepyx.dev@gmail.com')
+@pytest.fixture
+def session(reg_a):
+    sess = reg_a.earthdata_login('icepyx_devteam', 'icepyx.dev@gmail.com')
     getpass.return_value = os.getenv('NSIDC_LOGIN')
+    return sess
+        
 #    with mock.patch.object(builtins, 'input', lambda _: os.getenv('NSIDC_LOGIN')):
 
-
-def test_download_granules_with_subsetting():
+def test_download_granules_with_subsetting(reg_a, session):
     path = './downloads_subset'
     reg_a.order_granules(session)
     reg_a.download_granules(session,path)
@@ -29,7 +34,7 @@ def test_download_granules_with_subsetting():
 #         obs == exp
 
         
-def test_download_granules_without_subsetting():
+def test_download_granules_without_subsetting(reg_a, session):
     path = './downloads'
     reg_a.order_granules(session, subset=False)
     reg_a.download_granules(session, path)

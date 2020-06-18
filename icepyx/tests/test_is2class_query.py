5e4187a02cd32e591dff5d3bf313e76937ff8260
Bruce Wallin <bruce.wallin@nsidc.org>
2020-06-18 04:13:39 -0600

    Rename and update tests

--- icepyx/tests/test_is2class_query.py
+++ icepyx/tests/test_is2class_query.py
@@ -1,10 +1,9 @@
-from icepyx import is2class as ipd
+import icepyx.core.icesat2data as ipd
 import pytest
 import warnings
 
 def test_CMRparams():
     reg_a = ipd.Icesat2Data('ATL06',[-64, 66, -55, 72],['2019-02-22','2019-02-28'])
-    reg_a.build_CMR_params()
     obs_keys = reg_a.CMRparams.keys()
     exp_keys_all = ['short_name','version','temporal']
     exp_keys_any = ['bounding_box','polygon']
@@ -16,17 +15,14 @@ def test_reqconfig_params():
     reg_a = ipd.Icesat2Data('ATL06',[-64, 66, -55, 72],['2019-02-22','2019-02-28'])
     
     #test for search params
-    reg_a.build_reqconfig_params('search')
     obs_keys = reg_a.reqparams.keys()
     exp_keys_all = ['page_size','page_num']    
     assert all(keys in obs_keys for keys in exp_keys_all)
 
     #test for download params
-    reg_a.reqparams=None
-    reg_a.build_reqconfig_params('download')
     reg_a.reqparams.update({'token':'','email':''})
     obs_keys = reg_a.reqparams.keys()
-    exp_keys_all = ['page_size','page_num','request_mode','token','email','include_meta']
+    exp_keys_all = ['page_size','page_num','token','email']
     assert all(keys in obs_keys for keys in exp_keys_all)
     
 def test_properties():

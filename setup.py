4f5cd54986dd5b70489fef4d3456420811a6f677
JessicaS11 <jbscheick@gmail.com>
2020-08-18 15:48:21 +0000

    fix version in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="icepyx",
-    version="0.0.2",
+    version="0.3.0",
     author="The icepyx Developers",
     author_email="jbscheick@gmail.com",
     maintainer="Jessica Scheick",

96a2facedc40b27fd106ca92f23797b40f8fd863
Jessica <JessicaS11@users.noreply.github.com>
2021-01-20 10:30:17 -0500

    re-fix socket error (#163)
    See #98, which was partly undone by #96.

--- icepyx/core/Earthdata.py
+++ icepyx/core/Earthdata.py
@@ -45,15 +45,12 @@ class Earthdata:
     def _start_session(self):
         # Request CMR token using Earthdata credentials
         token_api_url = "https://cmr.earthdata.nasa.gov/legacy-services/rest/tokens"
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
         # try initially with machine hostname
         # revert to using localhost if gaierror exception
         try:
             ip = socket.gethostbyname(socket.gethostname())
         except:
             ip = socket.gethostbyname("localhost")
-
 
         data = {
             "token": {

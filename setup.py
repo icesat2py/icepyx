30700d73505381bc7f90816f0884a4754ad72ea9
Wei Ji <23487320+weiji14@users.noreply.github.com>
2021-03-06 05:40:42 +1300

    Setup GitHub Action for publishing to PyPI and TestPyPI (#174)
    * Setup Github Action for publishing to PyPI and TestPyPI
    
    Publishing the icepyx package to PyPI on every tagged (published) release. Every push to the development branch is also published to TestPyPI for testing purposes.
    
    * Remove PyPI/TestPyPI deploying steps from .travis.yml
    
    * Trigger publishing to TestPyPI manually using workflow_dispatch

--- setup.py
+++ setup.py
@@ -27,8 +27,10 @@ setuptools.setup(
         "Development Status :: 1 - Planning",
     ],
     use_scm_version={
+        "fallback_version": "unknown",
+        "local_scheme": "node-and-date",
         "write_to": "_version.py",
         "write_to_template": 'version = "{version}"\n',
     },
-    setup_requires=["setuptools>=30.3.0", "wheel", "setuptools_scm"]
+    setup_requires=["setuptools>=30.3.0", "wheel", "setuptools_scm"],
 )

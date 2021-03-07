8ed28d9c474b95154292379c220161daae92750c
Tian Li <tian.li@bristol.ac.uk>
2021-03-07 17:34:26 +0000

    Remove invaid OA request, use dask and xarray to speed up datamanipulation, enhance interactive visualization based on cycle number

--- icepyx/core/visualization.py
+++ icepyx/core/visualization.py
@@ -1,166 +1,178 @@
 # utility modules
-import glob
-import os
-import sys
-import re
 import intake
 import requests
 import numpy as np
-import pandas as pd
-import geopandas as gpd
-from pyproj import CRS
-from shapely.geometry import Point
+from shapely.geometry import Polygon
 from itertools import compress
 import concurrent.futures
-import matplotlib.pyplot as plt
-import holoviews as hv
+from tqdm import tqdm
+import xarray as xr
 import dask.array as da
-import dask.dataframe as dd
-import hvplot.dask  # adds hvplot method to dask objects
 import datashader as ds
-from holoviews import opts
+import geoviews as gv
+import holoviews as hv
 from holoviews.operation.datashader import datashade, rasterize
-#from holoviews.element.tiles import EsriImager
-hv.extension('bokeh', "matplotlib") 
 
+import icepyx as ipx
 import icepyx.core.geospatial as geospatial
+
+hv.extension('bokeh')
+gv.extension('bokeh')
 
 def filelist_latestcycle(filelist, cycle_list):
     """
     Only get filelist for lastest cycle if multiple cycles are requested
     """
-    if len(cycle_list) >1:
-    # for multiple repeat cycles, only request data for lastest cycle
+    if len(cycle_list) > 1:
+        # for multiple repeat cycles, only request data for lastest cycle
         latest_cycle = max(cycle_list)
-        new_filelist = [f for f in filelist if int(f[-14:-12])==latest_cycle]
+        new_filelist = [f for f in filelist if int(f[-14:-12]) == latest_cycle]
         return new_filelist
 
-    else: # only one cycle
+    else:
         return filelist
 
+
 class Visualize:
-    
-    """
-    Visualize data extent and the elevation distribution by requesting OpenAltimetry API before downloading data from NSIDC
-    """
-    
-    def __init__(self, filelist, bbox, product):
-        
-        self.filelist = filelist # filelist requested from icepyx Query object
-        self.bbox = bbox # Input bounding box 
-        self.product = product # Input ICESat-2 product
 
-    def file_meta(self,bbox_i):
+    def __init__(self, product, bbox, date_range):
+        self.product = product
+        self.bbox = bbox
+        self.date_range = date_range
+
+    def grid_bbox(self, binsize=5):
         """
-        Obtain metadata from filename list with bounding box satistifying OA 5 degree threshold
-        Return:
-            para_lists
-            
-        ### NEED CHANGE HERE ###
+        Split bounding box into 5 x 5 grids
+        when latitude/longitude range exceeds the default OpenAltimetry 5*5 degree spatial limits
+        :return: a list of 5*5 degree bbox
         """
-
-        para_lists = [] # list of parameters for API query
-
-        for fname in self.filelist:
-
-            if self.product in ['ATL07', 'ATL10']:
-                temp = intake.source.utils.reverse_format(
-                        format_string="{product:5d}-{HH:02d}_{datetime:%Y%m%d%H%M%S}_{rgt:04d}{cycle:02d}{region:02d}_{release:03d}_{version:02d}.h5",
-                        resolved_string=fname,
-                    )
-
-            else: 
-                temp = intake.source.utils.reverse_format(
-                        format_string="{product:5d}_{datetime:%Y%m%d%H%M%S}_{rgt:04d}{cycle:02d}{region:02d}_{release:03d}_{version:02d}.h5",
-                        resolved_string=fname,
-                    )
-
-            rgt = temp['rgt']
-            cycle = temp['cycle']
-            f_date = str(temp['datetime'].date())
-
-            paras = [rgt, f_date, cycle, bbox_i, self.product]
-            para_lists.append(paras)
-
-        return para_lists
-
-    def grid_bbox(self, binsize = 5):
-        """Split bounding box into smaller grids if latitude/longitude range exceed the default 5 degree limit of OpenAltimetry """
-
         lonmin = self.bbox[0]
         latmin = self.bbox[1]
         lonmax = self.bbox[2]
         latmax = self.bbox[3]
 
-        lonx = np.arange(lonmin, lonmax + binsize, binsize)
-        laty = np.arange(latmin, latmax + binsize, binsize) 
-        lonxv, latyv = np.meshgrid(lonx, laty)
+        # check if bounding box satisfies extent thresholds:
+        # |maxx - minx| < 5 and maxy - miny < 5
+        split_flag = ((lonmax - lonmin) > 5) or ((latmax - latmin) > 5)
 
-        ydim, xdim = lonxv.shape 
+        if split_flag:
+            # split bounding box into a list of 5*5 bbox
+            lonx = np.arange(lonmin, lonmax + binsize, binsize)
+            laty = np.arange(latmin, latmax + binsize, binsize)
+            lonxv, latyv = np.meshgrid(lonx, laty)
 
-        bbox_list = []
-        for i in np.arange(0,ydim-1): 
-            for j in np.arange(0,xdim-1):
-                # iterate grid for bounding box
-                bbox_ij = [lonxv[i,j], latyv[i,j], lonxv[i+1,j+1], latyv[i+1,j+1]]
+            ydim, xdim = lonxv.shape
 
-                if bbox_ij[2] > lonmax:
-                    bbox_ij[2] = lonmax
+            bbox_list = []
+            for i in np.arange(0, ydim - 1):
+                for j in np.arange(0, xdim - 1):
+                    # iterate grid for bounding box
+                    bbox_ij = [lonxv[i, j], latyv[i, j], lonxv[i + 1, j + 1], latyv[i + 1, j + 1]]
 
-                if bbox_ij[3] > latmax:
-                    bbox_ij[3] = latmax
+                    if bbox_ij[2] > lonmax:
+                        bbox_ij[2] = lonmax
 
-                bbox_list.append(bbox_ij)
+                    if bbox_ij[3] > latmax:
+                        bbox_ij[3] = latmax
 
-        return bbox_list
+                    bbox_list.append(bbox_ij)
 
-    def generate_OA_paras(self):
-        """Generate parameter lists for OpenAltimetry API"""
+            return bbox_list
 
-        # spatial extent check and grid bbox
-        lonmax = self.bbox[2]
-        lonmin = self.bbox[0]
-        latmax = self.bbox[3]
-        latmin = self.bbox[1]
-
-        # check if bounding box satisfies extent thresholds: |maxx - minx| < 5 and maxy - miny < 5 ###
-        extent_flag = ((lonmax - lonmin) > 5 ) or ((latmax - latmin) > 5 ) 
-
-        if extent_flag:
-            #print ('spatial extent exceeds OA 5 degree threshold, spliting bbox...')
-            bbox_list = self.grid_bbox()
-            paras = [self.file_meta(bbox_i) for bbox_i in bbox_list]
-            para_lists = [para_list for sublists in paras for para_list in sublists]
         else:
-            #print ('spatial extent satifies OA 5 degree threshold, generating data...')
-            bbox_list = self.bbox
-            para_lists = self.file_meta(bbox_list)
+            return [self.bbox]
 
-        return para_lists
+    def query_icesat2_filelist(self):
+        """
+        Query list of ICESat-2 files based on splitted bbox
+        :return: tuples of non-empty bbox list and corresponding ICESat-2 file lists
+        """
+        # a list of 5*5 bounding boxes
+        bbox_list = self.grid_bbox()
 
-    def request_OA_elev(self, paralist):
+        is2_bbox_list = []
+        is2_file_list = []
+
+        for bbox_i in bbox_list:
+            ### NEED Change for Track & Cycle Implementation
+            region = ipx.Query(self.product, bbox_i, self.date_range)
+            icesat2_files = region.avail_granules(ids=True, cycles=True, tracks=True)[0]
+
+            if not icesat2_files:
+                continue
+
+            is2_bbox_list.append(bbox_i)
+            is2_file_list.append(icesat2_files)
+
+        filelist_tuple = zip(is2_bbox_list, is2_file_list)
+
+        return filelist_tuple
+
+    def generate_OA_parameters(self):
         """
-        Request data from OpenAltimetry based on API: https://openaltimetry.org/data/swagger-ui/#/
-        Return:
-            OA_array: numpy array for all beams of one RGT in paralist
+        Get metadata from file lists in each 5*5 bbox
+        :return: a list of parameters for OpenAltimetry API query, including
+                 RGT, cycle number, datetime, bounding box, product name
         """
+        # list of parameters for API query
+        paras_list = []
+
+        # t uple of non-empty bbox and ICESat-2 datafiles
+        filelist_tuple = self.query_icesat2_filelist()
+
+        for bbox_i, filelist in filelist_tuple:
+
+            for fname in filelist:
+
+                if self.product in ['ATL07', 'ATL10']:
+                    temp = intake.source.utils.reverse_format(
+                        format_string="{product:5d}-{HH:02d}_{datetime:%Y%m%d%H%M%S}_{rgt:04d}{cycle:02d}{"
+                                      "region:02d}_{release:03d}_{version:02d}.h5",
+                        resolved_string=fname,
+                    )
+
+                else:
+                    temp = intake.source.utils.reverse_format(
+                        format_string="{product:5d}_{datetime:%Y%m%d%H%M%S}_{rgt:04d}{cycle:02d}{region:02d}_{"
+                                      "release:03d}_{version:02d}.h5",
+                        resolved_string=fname,
+                    )
+
+                rgt = temp['rgt']  # Reference Ground Track
+                cycle = temp['cycle']  # Cycle number
+                f_date = str(temp['datetime'].date())  # Datetime
+
+                paras = [rgt, f_date, cycle, bbox_i, self.product]
+                paras_list.append(paras)
+
+        return paras_list
+
+    def request_OA_data(self, paras):
+        """
+        Request data from OpenAltimetry based on API:
+        https://openaltimetry.org/data/swagger-ui/#/
+
+        :param paras: one parameter list for OpenAltimetry API request
+        :return: xarray dataset
+        """
+
         base_url = 'https://openaltimetry.org/data/api/icesat2/level3a'
 
-        points = [] # store all beam data for one RGT
+        trackId = paras[0]
+        Date = paras[1]
+        cycle = paras[2]
+        bbox = paras[3]
+        product = paras[4]
 
-        trackId,Date,cycle,bbox,product = paralist[0], paralist[1], paralist[2], paralist[3], paralist[4]
-
-        # iterate all six beams 
-        # for beam in beamlist:
         # Generate API
-        payload =  {'product':product.lower(),
-                    'endDate': Date,
-                    'minx':str(bbox[0]),
-                    'miny':str(bbox[1]),
-                    'maxx':str(bbox[2]),
-                    'maxy':str(bbox[3]),
-                    'trackId': trackId,
-                    'outputFormat':'json'} # default return all six beams
+        payload = {'product': product.lower(),
+                   'endDate': Date,
+                   'minx': str(bbox[0]),
+                   'miny': str(bbox[1]),
+                   'maxx': str(bbox[2]),
+                   'maxy': str(bbox[3]),
+                   'trackId': trackId,
+                   'outputFormat': 'json'}  # default return all six beams
 
         # request OpenAltimetry
         r = requests.get(base_url, params=payload)
@@ -171,114 +183,130 @@ class Visualize:
         # length of file list
         file_len = len(elevation_data['data'])
 
-        # file index satifies aqusition time from data file
+        # file index satisfies acquisition time from data file
         idx = [elevation_data['data'][i]['date'] == Date for i in np.arange(file_len)]
 
         # get data we need
         beam_data = list(compress(elevation_data['data'], idx))
 
         if not beam_data:
-            return 
+            return
 
         data_name = 'lat_lon_elev'
 
         if product == 'ATL08':
             data_name = 'lat_lon_elev_canopy'
 
-        # elevation array by iterating all six beams
-        beam_elev = [beam_data[0]['beams'][i][data_name] for i in np.arange(6) if beam_data[0]['beams'][i][data_name] != []]
+        # iterate six beams
+        beam_elev = [beam_data[0]['beams'][i][data_name] for i in np.arange(6) if
+                     beam_data[0]['beams'][i][data_name] != []]
 
-        if not beam_elev: # check if no data 
-            return  
+        if not beam_elev:  # if no data
+            return
 
+        # elevation for all available beams
         OA_array = np.vstack(beam_elev)
 
         if OA_array.size > 0:
-            return OA_array
+            # convert numpy to dask array
+            OA_array = np.expand_dims(OA_array, axis=(0, 1, 2))
+            OA_darr = da.from_array(OA_array, chunks=5000)
 
+            elevation = OA_darr[:, :, :, :, 2]
+            lat = OA_darr[:, :, :, :, 0]
+            lon = OA_darr[:, :, :, :, 1]
+            index = np.arange(elevation.shape[3])
+            bbox_no = [str(bbox)]
+            rgt_no = [trackId]
+            cycle_no = [cycle]
+
+            # create xarray dataset
+            OA_ds = xr.Dataset(
+                data_vars=dict(
+                    elevation=(["rgt", "cycle", "bbox", "index"], elevation),
+                    lat=(["rgt", "cycle", "bbox", "index"], lat),
+                    lon=(["rgt", "cycle", "bbox", "index"], lon)
+                ),
+                coords=dict(
+                    rgt=rgt_no,
+                    cycle=cycle_no,
+                    index=index,
+                    bbox=bbox_no
+                ),
+                attrs=dict(description="ICESat-2 Elevation"),
+
+            )
+            return OA_ds
 
     def parallel_request_OA(self):
-        """ 
+        """
         Requesting elevation data from OpenAltimetry API in parallel
-        Only supports OA_Products ['ATL06','ATL07','ATL08','ATL10','ATL12','ATL13']
-        For ATL03 Photon Data, OA only supports single date request according to: https://openaltimetry.org/data/swagger-ui/#/Public/getATL08DataByDate, 
-        and the geospatial limitation is 1 degree in both lat/lon
-        
-        Return: 
-            OA_data: dataframe containing all requested OA datasets
+        Now only supports OA_Products ['ATL06','ATL07','ATL08','ATL10','ATL12','ATL13']
+
+        For ATL03 Photon Data, OA only supports single date request
+        according to: https://openaltimetry.org/data/swagger-ui/#/Public/getATL08DataByDate,
+        with geospatial limitation of 1 degree lat/lon
+
+        :return: all requested ICESat-2 elevation in xarray dataset
         """
 
-        print ('Requesting data for elevation visualization, please wait...')
+        print('Generate parameters')
 
         # generate parameter lists for OA requesting
-        para_lists = self.generate_OA_paras()
+        OA_para_list = self.generate_OA_parameters()
+
+        print('Request data from OpenAltimetry, please wait...')
 
         ### Parallel processing ###
         pool = concurrent.futures.ThreadPoolExecutor(max_workers=30)
-        parallel_OA_data = {pool.submit(self.request_OA_elev,paralist): paralist for paralist in para_lists}
+        parallel_OA_data = {pool.submit(self.request_OA_data, para_list): para_list for para_list in OA_para_list}
 
-        requested_OA_data=[]
+        requested_OA_data = []
 
-        for future in concurrent.futures.as_completed(parallel_OA_data):
+        # for future in tqdm(concurrent.futures.as_completed(parallel_OA_data)):
+        for future in tqdm(
+            iterable=concurrent.futures.as_completed(parallel_OA_data),
+            total=len(parallel_OA_data),
+        ):
             r = future.result()
             if r is not None:
                 requested_OA_data.append(r)
 
         try:
-            OA_data = np.vstack(requested_OA_data) 
-            return OA_data
+            # OA_data = np.vstack(requested_OA_data)
+            OA_data_ds = xr.merge(list(filter(None, requested_OA_data)))
+            return OA_data_ds
 
         except:
             pass
 
-    def visualize_elevation_matplotlib(self, OA_data):
-        """Visualize elevation data requested from OpenAltimetry API"""
+    def visualize_extent(self):
+        """Show bounding box extent set by users"""
+        region = ipx.Query(self.product, self.bbox, self.date_range)
+        gdf = geospatial.geodataframe(region.extent_type, region._spat_extent)
+        line_geoms = Polygon(gdf['geometry'][0]).boundary
+        bbox_poly = gv.Path(line_geoms).opts(color="red", line_color="red")
+        tile = gv.tile_sources.EsriImagery.opts(width=500, height=500)
+        return tile * bbox_poly
 
-        # convert into geodataframe
-        geometry = gpd.points_from_xy(x=OA_data[:,1], y=OA_data[:,0])     
-        gdf_OA = gpd.GeoDataFrame({'lat':OA_data[:,0],
-                                   'lon':OA_data[:,1],
-                                   'h':OA_data[:,2]}, 
-                                  geometry=geometry, 
-                                  crs=CRS('EPSG:4326'))
-
-        # plot elevation
-        fig= plt.figure(figsize=(8, 8))
-        ax = fig.add_subplot(111)
-        cs = plt.scatter(gdf_OA['geometry'].x, gdf_OA['geometry'].y, c=gdf_OA['h'], vmin=gdf_OA['h'].min(),
-                         vmax=gdf_OA['h'].max(), cmap='RdYlBu_r', s=15, label='OpenAltimetry')
-
-        colorbar = fig.colorbar(cs, ax=ax)
-        colorbar.set_label('Elevation(m)', fontsize=15)
-        colorbar.ax.tick_params(labelsize=15)
-
-        plt.tight_layout()
-        plt.show()
-
-    def visualize_elevations_holoview(self, OA_data):
+    def visualize_elevation(self):
         """
-        Visualize elevation data requested from OpenAltimetry API
+        Visualize elevation requested from OpenAltimetry API using datashader based on cycles
         https://holoviz.org/tutorial/Large_Data.html
-        https://datashader.org/getting_started/Interactivity.html
-        https://towardsdatascience.com/data-visualization-with-python-holoviz-plotting-4848e905f2c0
         """
-        # convert array into dask dataframe
-        oa_array = da.from_array(OA_data, chunks=(5000, 3))
-        oa_df = dd.from_dask_array(oa_array, columns=['lat', 'lon', 'h'])
-        x, y = ds.utils.lnglat_to_meters(oa_df.lon, oa_df.lat)
-        ddf = oa_df.assign(x=x, y=y).persist()
-        points = hv.Points(ddf, ['x', 'y'])
-        tiles = hv.element.tiles.EsriImagery().opts(xaxis=None, yaxis=None, width=700, height=500)
-        return tiles * datashade(points)
 
-    def visualize_spatial_extent(self, extent_type, spat_extent):
-        """Default visualization of spatial extent"""
+        OA_ds  = self.parallel_request_OA()
+        ddf = OA_ds.to_dask_dataframe()
+        ddf = ddf.repartition(npartitions=ddf.npartitions * 1000)
 
-        world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
-        f, ax = plt.subplots(1, figsize=(12, 6))
-        world.plot(ax=ax, facecolor="lightgray", edgecolor="gray")
-        geospatial.geodataframe(extent_type, spat_extent).plot(
-            ax=ax, color="#FF8C00", alpha=0.7
-        )
-        plt.show()
-    
+        print ('Plot elevation, please wait...')
+
+        x, y = ds.utils.lnglat_to_meters(ddf.lon, ddf.lat)
+        ddf_new = ddf.assign(x=x, y=y).persist()
+
+        dset = hv.Dataset(ddf_new)
+        ropts = dict(colorbar=True, tools=["hover"], width=350)
+        grouped = dset.to(hv.Scatter, ['x'], vdims=['y', 'elevation'], groupby=['cycle'], dynamic=True)
+        tiles = hv.element.tiles.EsriImagery().opts(xaxis=None, yaxis=None, width=500, height=500)
+
+        return tiles * rasterize(grouped).opts(cmap="cool", cnorm="linear").opts(**ropts)
--- icepyx/tests/test_OpenAltimetry.py
+++ icepyx/tests/test_OpenAltimetry.py
@@ -1,22 +1,11 @@
-import icepyx as ipx
-from icepyx.core.visualization import Visualize, filelist_latestcycle
-
+from icepyx.core.visualization import Visualize
 
 def test_OA_request():
-    
     short_name = 'ATL06'
-    date_range = ['2019-4-14','2019-7-1']
-    bbox = [2, -73, 10, -70]
-    region = ipx.Query(short_name, bbox, date_range)
-    filelist_nsidc = region.avail_granules(ids=True, cycles=True, tracks=True)[0]
-    product = region.dataset
-    cycle_list  = list(map(int, set(region.avail_granules(ids=True, cycles=True, tracks=True)[1])))
-    filelist = filelist_latestcycle(filelist_nsidc, cycle_list)
-    OA_viz = Visualize(filelist, bbox, product)
-    oa_data = OA_viz.parallel_request_OA()
-    OA_viz.visualize_elevations_holoview(oa_data)
-    
-    assert oa_data.empty == False
+    date_range = ['2019-4-14', '2019-4-30']
+    bbox = [-58, -67, -56, -61]
 
+    region_viz = Visualize(short_name, bbox, date_range)
+    output_ds = region_viz.parallel_request_OA()
 
-
+    assert output_ds.elevation.size > 0

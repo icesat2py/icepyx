552fd3f0c2a8932af153ddbcb050a6a7488eb90a
Jessica Scheick <jbscheick@gmail.com>
2021-07-27 16:51:28 -0400

    separate intake catalog functions to another module

---
+++ icepyx/core/is2cat.py
@@ -0,0 +1,156 @@
+from intake.catalog import Catalog
+
+
+# next steps:
+# add step that gets icesat2 data product and uses it to get var_path and var_path_param dicts that correspond to the default subset variables
+# then, write a reader function that calls intake (with the right catalog, if it wasn't specifically created or supplied) and merges the beams/spots
+
+# Need to post on intake's page to see if this would be a useful contribution...
+# https://github.com/intake/intake/blob/master/intake/source/utils.py#L216
+def _pattern_to_glob(pattern):
+    """
+    Adapted from intake.source.utils.path_to_glob to convert a path as pattern into a glob style path
+    that uses the pattern's indicated number of '?' instead of '*' where an int was specified.
+
+    Returns pattern if pattern is not a string.
+
+    Parameters
+    ----------
+    pattern : str
+        Path as pattern optionally containing format_strings
+
+    Returns
+    -------
+    glob : str
+        Path with int format strings replaced with the proper number of '?' and '*' otherwise.
+
+    Examples
+    --------
+    >>> _pattern_to_glob('{year}/{month}/{day}.csv')
+    '*/*/*.csv'
+     >>> _pattern_to_glob('{year:4}/{month:2}/{day:2}.csv')
+    '????/??/??.csv'
+    >>> _pattern_to_glob('data/{year:4}{month:02}{day:02}.csv')
+    'data/????????.csv'
+    >>> _pattern_to_glob('data/*.csv')
+    'data/*.csv'
+    """
+    from string import Formatter
+
+    if not isinstance(pattern, str):
+        return pattern
+
+    fmt = Formatter()
+    glob = ""
+    prev_field_name = None
+    for literal_text, field_name, format_specs, _ in fmt.parse(pattern):
+        glob += literal_text
+        if field_name and (glob[-1] != "*"):
+            try:
+                glob += "?" * int(format_specs)
+            except ValueError:
+                glob += "*"
+                # alternatively, you could use bits=utils._get_parts_of_format_string(resolved_string, literal_texts, format_specs)
+                # and then use len(bits[i]) to get the length of each format_spec
+    # print(glob)
+    return glob
+
+
+def build_catalog(
+    data_source,
+    path_pattern,
+    source_type,
+    var_paths=None,
+    var_path_params=None,
+    **kwargs
+):
+    """
+    Build an Intake catalog for reading in ICESat-2 data.
+
+    Parameters
+    ----------
+    var_paths : str, default None
+        Variable paths to load.
+        Can include general parameter names, which must be contained within double curly brackets and further
+        described in `var_path_params`.
+        Default list based on data product of provided files.
+        If multiple data products are included in the files, the default list will be for the product of the first file.
+        This may result in errors during read-in if all files do not have the same variable paths.
+
+    var_path_params : [dict], default None
+        List of dictionaries with a keyword for each parameter name specified in the `var_paths` string.
+        Each parameter keyword should contain a dictionary with the acceptable keyword-value pairs for the driver being used.
+
+    **kwargs :
+        Keyword arguments to be passed through to `intake.catalog.Catalog.from_dict()`.
+        Keywords needed to override default inputs include:
+            - `source_args_dict` # highest level source information; keys include: "urlpath", "path_as_pattern", driver-specific ("xarray_kwargs" is default)
+            - `metadata_dict`
+            - `source_dict` # individual source entry  information (default is supplied by data object; "name", "description", "driver", "args")
+            - `defaults_dict`  # catalog "name", "description", "metadata", "entries", etc.
+
+    Returns
+    -------
+    intake.catalog.Catalog object accessible via self.catalog
+
+    Examples
+    --------
+    >>> reader = icepyx.read.Read("/full/path/to/set/of/ICESat-2/ATL06/files")
+
+    >>> reader.build_catalog(var_paths="/gt1l/land_ice_segments")
+    <>
+
+    >>> reader.build_catalog(var_paths = "", var_path_params = )
+
+    """
+    from intake.catalog.local import LocalCatalogEntry, UserParameter
+    import intake_xarray
+
+    import icepyx.core.APIformatting as apifmt
+
+    assert var_paths, "You must enter a variable path."
+
+    # generalize this/make it so the values can be entered as kwargs...
+    xarray_kwargs_dict = {"engine": "h5netcdf", "group": var_paths}
+
+    source_args_dict = {
+        "urlpath": data_source,
+        "path_as_pattern": path_pattern,
+        "xarray_kwargs": xarray_kwargs_dict,
+    }
+
+    metadata_dict = {"version": 1}
+
+    source_dict = {
+        "name": source_type,
+        "description": "",
+        "driver": "intake_xarray.netcdf.NetCDFSource",  # NOTE: this must be a string or the catalog cannot be imported after saving
+        "args": source_args_dict,
+    }
+
+    if var_path_params:
+        source_dict = apifmt.combine_params(
+            source_dict,
+            {"parameters": [UserParameter(**params) for params in var_path_params]},
+        )
+
+    # NOTE: LocalCatalogEntry has some required positional args (name, description, driver)
+    local_cat_source = {
+        source_type: LocalCatalogEntry(
+            name=source_dict.pop("name"),
+            description=source_dict.pop("description"),
+            driver=source_dict.pop("driver"),
+            **source_dict,
+        )
+    }
+
+    defaults_dict = {
+        "name": "IS2-hdf5-icepyx-intake-catalog",
+        "description": "an icepyx-generated catalog for creating local ICESat-2 intake entries",
+        "metadata": metadata_dict,
+        "entries": local_cat_source,
+    }
+
+    build_cat_dict = apifmt.combine_params(defaults_dict, kwargs)
+
+    return Catalog.from_dict(**build_cat_dict)
--- icepyx/core/read.py
+++ icepyx/core/read.py
@@ -1,7 +1,8 @@
 import fnmatch
-from intake.catalog import Catalog
 import os
 import xarray as xr
+
+from icepyx.core import is2cat
 
 # from icepyx.core.query import Query
 
@@ -39,57 +40,6 @@ def _validate_source(source):
         os.path.isdir(source) == True or os.path.isfile(source) == True
     ), "Your data source string is not a valid data source."
     return True
-
-
-# Need to post on intake's page to see if this would be a useful contribution...
-# https://github.com/intake/intake/blob/master/intake/source/utils.py#L216
-def _pattern_to_glob(pattern):
-    """
-    Adapted from intake.source.utils.path_to_glob to convert a path as pattern into a glob style path
-    that uses the pattern's indicated number of '?' instead of '*' where an int was specified.
-
-    Returns pattern if pattern is not a string.
-
-    Parameters
-    ----------
-    pattern : str
-        Path as pattern optionally containing format_strings
-
-    Returns
-    -------
-    glob : str
-        Path with int format strings replaced with the proper number of '?' and '*' otherwise.
-
-    Examples
-    --------
-    >>> _pattern_to_glob('{year}/{month}/{day}.csv')
-    '*/*/*.csv'
-     >>> _pattern_to_glob('{year:4}/{month:2}/{day:2}.csv')
-    '????/??/??.csv'
-    >>> _pattern_to_glob('data/{year:4}{month:02}{day:02}.csv')
-    'data/????????.csv'
-    >>> _pattern_to_glob('data/*.csv')
-    'data/*.csv'
-    """
-    from string import Formatter
-
-    if not isinstance(pattern, str):
-        return pattern
-
-    fmt = Formatter()
-    glob = ""
-    prev_field_name = None
-    for literal_text, field_name, format_specs, _ in fmt.parse(pattern):
-        glob += literal_text
-        if field_name and (glob[-1] != "*"):
-            try:
-                glob += "?" * int(format_specs)
-            except ValueError:
-                glob += "*"
-                # alternatively, you could use bits=utils._get_parts_of_format_string(resolved_string, literal_texts, format_specs)
-                # and then use len(bits[i]) to get the length of each format_spec
-    # print(glob)
-    return glob
 
 
 def _run_fast_scandir(dir, fn_glob):
@@ -136,7 +86,7 @@ def _check_source_for_pattern(source, filename_pattern):
     """
     Check that the entered data source contains files that match the input filename_pattern
     """
-    glob_pattern = _pattern_to_glob(filename_pattern)
+    glob_pattern = is2cat._pattern_to_glob(filename_pattern)
 
     if os.path.isdir(source):
         _, filelist = _run_fast_scandir(source, glob_pattern)
@@ -235,106 +185,17 @@ class Read:
         --------
         >>>
         """
-        if not hasattr(self, "_catalog"):
+        if not hasattr(self, "_catalog") and hasattr(self, "_catalog_path"):
             from intake import open_catalog
 
             self._catalog = open_catalog(self._catalog_path)
 
+        else:
+            self._catalog = is2cat.build_catalog(
+                self.data_source, self._pattern, self._source_type
+            )
+
         return self._catalog
-
-    # ----------------------------------------------------------------------
-    # Methods
-    def build_catalog(
-        self, var_paths="/gt1l/land_ice_segments", var_path_params=None, **kwargs
-    ):
-        """
-        Build an Intake catalog for reading in ICESat-2 data.
-
-        Parameters
-        ----------
-        var_paths : str
-            Variable paths to load.
-            Can include general parameter names, which must be contained within double curly brackets and further
-            described in `var_path_params`.
-            Default list based on data product of provided files.
-            If multiple data products are included in the files, the default list will be for the product of the first file.
-            This may result in errors during read-in if all files do not have the same variable paths.
-
-        var_path_params : [dict], default None
-            List of dictionaries with a keyword for each parameter name specified in the `var_paths` string.
-            Each parameter keyword should contain a dictionary with the acceptable keyword-value pairs for the driver being used.
-
-        **kwargs :
-            Keyword arguments to be passed through to `intake.catalog.Catalog.from_dict()`.
-            Keywords needed to override default inputs include:
-                - `source_args_dict` # highest level source information; keys include: "urlpath", "path_as_pattern", driver-specific ("xarray_kwargs" is default)
-                - `metadata_dict`
-                - `source_dict` # individual source entry  information (default is supplied by data object; "name", "description", "driver", "args")
-                - `defaults_dict`  # catalog "name", "description", "metadata", "entries", etc.
-
-        Returns
-        -------
-        intake.catalog.Catalog object accessible via self.catalog
-
-        Examples
-        --------
-        >>> reader = icepyx.read.Read("/full/path/to/set/of/ICESat-2/files")
-
-        >>> reader.build_catalog()
-        <>
-
-        >>> reader.build_catalog(var_paths = "", var_path_params = )
-
-        """
-        from intake.catalog.local import LocalCatalogEntry, UserParameter
-        import intake_xarray
-
-        import icepyx.core.APIformatting as apifmt
-
-        # generalize this/make it so the values can be entered as kwargs...
-        xarray_kwargs_dict = {"engine": "h5netcdf", "group": var_paths}
-
-        source_args_dict = {
-            "urlpath": self.data_source,
-            "path_as_pattern": self._pattern,
-            "xarray_kwargs": xarray_kwargs_dict,
-        }
-
-        metadata_dict = {"version": 1}
-
-        source_dict = {
-            "name": self._source_type,
-            "description": "",
-            "driver": "intake_xarray.netcdf.NetCDFSource",  # NOTE: this must be a string or the catalog cannot be imported after saving
-            "args": source_args_dict,
-        }
-
-        if var_path_params:
-            source_dict = apifmt.combine_params(
-                source_dict,
-                {"parameters": [UserParameter(**params) for params in var_path_params]},
-            )
-
-        # NOTE: LocalCatalogEntry has some required positional args (name, description, driver)
-        local_cat_source = {
-            self._source_type: LocalCatalogEntry(
-                name=source_dict.pop("name"),
-                description=source_dict.pop("description"),
-                driver=source_dict.pop("driver"),
-                **source_dict,
-            )
-        }
-
-        defaults_dict = {
-            "name": "IS2-hdf5-icepyx-intake-catalog",
-            "description": "an icepyx-generated catalog for creating local ICESat-2 intake entries",
-            "metadata": metadata_dict,
-            "entries": local_cat_source,
-        }
-
-        build_cat_dict = apifmt.combine_params(defaults_dict, kwargs)
-
-        self._catalog = Catalog.from_dict(**build_cat_dict)
 
 
 '''

ab68671641ff8cd07d88a14b1e2bdf64bd107163
Jessica Scheick <jbscheick@gmail.com>
2021-12-10 15:40:57 -0500

    fix query test that failed

--- icepyx/tests/test_query.py
+++ icepyx/tests/test_query.py
@@ -17,24 +17,23 @@ def test_icepyx_boundingbox_query():
         ["2019-02-22", "2019-02-28"],
         start_time="03:30:00",
         end_time="21:30:00",
-        version="4",
+        version="5",
     )
-    obs_list = [
+    obs_tuple = (
         reg_a.product,
         reg_a.dates,
         reg_a.start_time,
         reg_a.end_time,
         reg_a.product_version,
         reg_a.spatial_extent,
-    ]
-    exp_list = [
+    )
+    exp_tuple = (
         "ATL06",
         ["2019-02-22", "2019-02-28"],
         "03:30:00",
         "21:30:00",
-        "004",
-        ["bounding box", [-64, 66, -55, 72]],
-    ]
+        "005",
+        ("bounding box", [-64.0, 66.0, -55.0, 72.0]),
+    )
 
-    for obs, expected in zip(obs_list, exp_list):
-        assert obs == expected
+    assert obs_tuple == exp_tuple

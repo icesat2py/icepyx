b1f1b80792287b63f3d67359efd36863b3440805
Jessica Scheick <jbscheick@gmail.com>
2021-12-14 16:47:50 -0500

    remove dev-notebooks from exclusion list since dir was removed

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -56,7 +56,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["**.ipynb_checkpoints", "dev-notebooks"]
+exclude_patterns = ["**.ipynb_checkpoints"]
 
 # location of master document (by default sphinx looks for contents.rst)
 master_doc = "index"

a8f9ccf7fcd88e76c62f0448e2478363e3685ebc
Jessica Scheick <jbscheick@gmail.com>
2021-12-20 11:10:33 -0500

    update product version in earthdata tests

--- icepyx/tests/test_Earthdata.py
+++ icepyx/tests/test_Earthdata.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-test_Earthdata.py (05/2021)
+test_Earthdata.py (12/2021)
 """
 import os
 import re
@@ -10,42 +10,51 @@ import shutil
 import getpass
 import warnings
 import posixpath
+
 # from icepyx.core.Earthdata import Earthdata
 
 # PURPOSE: test different authentication methods
 @pytest.fixture(scope="module", autouse=True)
 def setup_earthdata():
     # Before test - move .netrc file to a temporary place
-    netrc_file = os.path.join(os.path.expanduser('~'),'.netrc')
+    netrc_file = os.path.join(os.path.expanduser("~"), ".netrc")
     temprc_file = f"{netrc_file}.temp"
-    if os.access(netrc_file,os.F_OK):
-        shutil.move(netrc_file,temprc_file)
+    if os.access(netrc_file, os.F_OK):
+        shutil.move(netrc_file, temprc_file)
     yield
     # After test - move .netrc file back to original path
-    if os.access(temprc_file,os.F_OK):
-        shutil.move(temprc_file,netrc_file)
+    if os.access(temprc_file, os.F_OK):
+        shutil.move(temprc_file, netrc_file)
     else:
         os.remove(netrc_file)
 
-def capability_url(dataset,version):
-    return posixpath.join("https://n5eil02u.ecs.nsidc.org","egi",
-        "capabilities",f"{dataset}.{version}.xml")
 
-def test_environment(username,password,email):
-    url = capability_url("ATL06","003")
-    netrc_file = os.path.join(os.path.expanduser('~'),'.netrc')
-    assert not os.access(netrc_file,os.F_OK)
-    assert Earthdata(username,email,url).login(attempts=1)
+def capability_url(dataset, version):
+    return posixpath.join(
+        "https://n5eil02u.ecs.nsidc.org",
+        "egi",
+        "capabilities",
+        f"{dataset}.{version}.xml",
+    )
 
-def test_netrc(username,password,email):
+
+def test_environment(username, password, email):
+    url = capability_url("ATL06", "005")
+    netrc_file = os.path.join(os.path.expanduser("~"), ".netrc")
+    assert not os.access(netrc_file, os.F_OK)
+    assert Earthdata(username, email, url).login(attempts=1)
+
+
+def test_netrc(username, password, email):
     # append to netrc file and set permissions level
-    args = (username,'urs.earthdata.nasa.gov',password)
-    netrc_file = os.path.join(os.path.expanduser('~'),'.netrc')
-    with open(netrc_file,'a+') as f:
-        f.write('machine {1} login {0} password {2}\n'.format(*args))
+    args = (username, "urs.earthdata.nasa.gov", password)
+    netrc_file = os.path.join(os.path.expanduser("~"), ".netrc")
+    with open(netrc_file, "a+") as f:
+        f.write("machine {1} login {0} password {2}\n".format(*args))
         os.chmod(netrc_file, 0o600)
-    url = capability_url("ATL06","003")
-    assert Earthdata(username,email,url).login(attempts=0)
+    url = capability_url("ATL06", "005")
+    assert Earthdata(username, email, url).login(attempts=0)
+
 
 class Earthdata:
     """
@@ -73,12 +82,13 @@ class Earthdata:
         uid,
         email,
         capability_url,
-        pswd=os.environ.get('EARTHDATA_PASSWORD'),
+        pswd=os.environ.get("EARTHDATA_PASSWORD"),
     ):
 
         assert isinstance(uid, str), "Enter your login user id as a string"
-        assert re.match(r"[^@]+@[^@]+\.[^@]+", email), \
-            "Enter a properly formatted email address"
+        assert re.match(
+            r"[^@]+@[^@]+\.[^@]+", email
+        ), "Enter a properly formatted email address"
 
         self.netrc = None
         self.uid = uid
@@ -90,12 +100,14 @@ class Earthdata:
         """
         Checks supplied credentials versus environmental variables
         """
-        EARTHDATA_USERNAME = os.environ.get('EARTHDATA_USERNAME')
-        EARTHDATA_PASSWORD = os.environ.get('EARTHDATA_PASSWORD')
-        EARTHDATA_EMAIL = os.environ.get('EARTHDATA_EMAIL')
-        if ((self.uid == EARTHDATA_USERNAME) &
-            (self.email == EARTHDATA_EMAIL) &
-            (self.pswd == EARTHDATA_PASSWORD)):
+        EARTHDATA_USERNAME = os.environ.get("EARTHDATA_USERNAME")
+        EARTHDATA_PASSWORD = os.environ.get("EARTHDATA_PASSWORD")
+        EARTHDATA_EMAIL = os.environ.get("EARTHDATA_EMAIL")
+        if (
+            (self.uid == EARTHDATA_USERNAME)
+            & (self.email == EARTHDATA_EMAIL)
+            & (self.pswd == EARTHDATA_PASSWORD)
+        ):
             return True
         else:
             return False
@@ -116,7 +128,7 @@ class Earthdata:
                     break
                 except KeyError:
                     pass
-                if ((i+1) < attempts):
+                if (i + 1) < attempts:
                     self.uid = input("Please re-enter your Earthdata user ID: ")
                     self.pswd = getpass.getpass("Earthdata Login password: ")
             else:

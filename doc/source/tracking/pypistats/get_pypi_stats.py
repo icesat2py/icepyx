a60541ea07260d5ef55f9bc643c17d0c4fe95435
anubhav <anubhav@umd.edu>
2022-02-02 18:32:00 -0500

    reducing cron frequency

--- doc/source/tracking/pypistats/get_pypi_stats.py
+++ doc/source/tracking/pypistats/get_pypi_stats.py
@@ -8,7 +8,7 @@ trackpath = f'{cwd}/doc/source/tracking/pypistats/'
 downloadfn = "downloads_data.csv"
 sysdownloadfn = "sys_downloads_data.csv"
 
-downloads = pypistats.overall("icepyx", total=True, format="pandas").drop(columns=['percent'])
+downloads = pypistats.overall("icepyx", format="pandas").drop(columns=['percent'])
 downloads = downloads[downloads.category != "Total"]
 
 # try:
@@ -21,7 +21,7 @@ dl_data = downloads.merge(exist_downloads, how='outer',
 
 dl_data.to_csv(trackpath + downloadfn, index=False)
 
-sysdownloads = pypistats.system("icepyx", total=True, format="pandas").drop(columns=['percent'])
+sysdownloads = pypistats.system("icepyx", format="pandas").drop(columns=['percent'])
 sysdownloads = sysdownloads[sysdownloads.category != "Total"]
 # try:
 exist_sysdownloads = pd.read_csv(trackpath + sysdownloadfn)  # .drop(columns=['percent'])
@@ -38,5 +38,4 @@ dl_data = dl_data.groupby("category").get_group("without_mirrors").sort_values("
 
 chart = dl_data.plot(x="date", y="downloads", figsize=(10, 2),
                      label="Number of PyPI Downloads")
-chart.figure.show()
 chart.figure.savefig(trackpath + "downloads.svg")

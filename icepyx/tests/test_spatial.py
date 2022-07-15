6a4ee13ebb2a442a16fa2d2b7b9db858c2ab1ff5
sjh1024 <sdcajh@gmail.com>
2022-07-15 16:27:47 -0400

    trying to fix spatial tests on Travis CI

--- icepyx/tests/test_spatial.py
+++ icepyx/tests/test_spatial.py
@@ -235,14 +235,14 @@ def test_poly_list_auto_close():
 
 def test_poly_file_simple_one_poly():
 
-    poly_from_file = sp.Spatial(str(Path('../../doc/source/example_notebooks/supporting_files/simple_test_poly.gpkg').resolve()))
+    poly_from_file = sp.Spatial(str(Path('./doc/source/example_notebooks/supporting_files/simple_test_poly.gpkg').resolve()))
     #print(poly_from_file.extent_file)
     #print(poly_from_file.spatial_extent)
     expected_poly = Polygon([[-55, 68], [-55, 71], [-48, 71], [-48, 68], [-55, 68]])
 
     assert poly_from_file.extent_type == "polygon"
     assert poly_from_file.extent_file is not None
-    assert poly_from_file.extent_file == str(Path('../../doc/source/example_notebooks/supporting_files/simple_test_poly.gpkg').resolve())
+    assert poly_from_file.extent_file == str(Path('./doc/source/example_notebooks/supporting_files/simple_test_poly.gpkg').resolve())
     assert poly_from_file.spatial_extent == expected_poly
 
 
@@ -262,4 +262,4 @@ def test_bad_poly_inputfile_name_throws_error():
 
 def test_bad_poly_inputfile_type_throws_error():
     with pytest.raises(TypeError):
-        bad_input = sp.Spatial(str(Path('../tests/test_read.py').resolve()))
+        bad_input = sp.Spatial(str(Path('./tests/test_read.py').resolve()))

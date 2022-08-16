90d6f6db227b24720e65411b8555d926157ae6ac
sjh1024 <sdcajh@gmail.com>
2022-08-16 11:50:57 -0400

    temporal changes before travel

--- icepyx/tests/test_temporal.py
+++ icepyx/tests/test_temporal.py
@@ -139,7 +139,7 @@ def test_bad_dict_length():
 
 
 def test_range_str_bad_range():
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
         result = tp.Temporal({"start_date": "2020-01-01", "end_date": "2016-01-01"})
 
 # NOTE: Not testing bad datetime/time inputs because it is assumed the datetime library

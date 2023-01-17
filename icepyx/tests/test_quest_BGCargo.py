0af53d694ef5731b69fe5293ea1be5918f942776
Romina <romina.piunno@gmail.com>
2023-01-17 16:55:58 -0500

    assert bounding box type in Argo init, begin framework for unit tests

---
+++ icepyx/tests/test_quest_BGCargo.py
@@ -0,0 +1,16 @@
+import icepyx as ipx
+import pytest
+import warnings
+
+
+def test_available_profiles():
+	pass
+
+def test_no_available_profiles():
+	pass
+
+def test_valid_BGCparams():
+	pass
+
+def test_invalid_BGCparams():
+	pass
---
+++ icepyx/tests/test_quest_argo.py
@@ -0,0 +1,14 @@
+import icepyx as ipx
+import pytest
+import warnings
+
+
+def test_available_profiles():
+	pass
+
+def test_no_available_profiles():
+	pass
+
+def test_valid_spatialextent():
+	pass
+

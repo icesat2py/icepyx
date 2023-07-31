4602cdb83529c730063240728fa9ae82711318db
Romina <romina.piunno@gmail.com>
2023-07-31 13:39:22 -0400

    add prelim test case for adding to Argo df

--- icepyx/tests/test_argo.py
+++ icepyx/tests/test_argo.py
@@ -3,16 +3,25 @@ import pytest
 import warnings
 from icepyx.quest import Argo
 
-def test_validate_params():
+def test_merge_df():
 	reg_a = Argo([-150, 30, -120, 60], ["2022-06-07", "2022-06-14"])
-	param_list = ["down_irradiance412"]
+	param_list = ["salinity", "temperature","down_irradiance412"]
 	bad_param = ["up_irradiance412"]
 	# param_list = ["doxy"]
 
 	# reg_a.search_data(params=bad_param, printURL=True)
 
 	df = reg_a.get_dataframe(params=param_list)
-	assert 1==1
+	print(df.columns)
+	assert "down_irradiance412" in df.columns
+	assert "down_irradiance412_argoqc" in df.columns
+
+	df = reg_a.get_dataframe(["doxy"], keep_existing=True)
+	assert "doxy" in df.columns
+	assert "doxy_argoqc" in df.columns
+	assert "down_irradiance412" in df.columns
+	assert "down_irradiance412_argoqc" in df.columns
+
 
 def test_abc():
 	assert 1 == 1

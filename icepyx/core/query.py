f1fa0df15d9208641ed1bdfbe6a853cee19daf5d
Rachel Wegener <rachel.e.wegener@gmail.com>
2023-08-22 13:28:58 +0000

    alphabetize ordering

--- icepyx/core/query.py
+++ icepyx/core/query.py
@@ -10,17 +10,17 @@ import time
 import warnings
 
 import icepyx.core.APIformatting as apifmt
+from icepyx.core.auth import EarthdataAuthMixin
 import icepyx.core.granules as granules
 from icepyx.core.granules import Granules as Granules
 import icepyx.core.is2ref as is2ref
 
 # QUESTION: why doesn't from granules import Granules as Granules work, since granules=icepyx.core.granules?
 # from icepyx.core.granules import Granules
-from icepyx.core.variables import Variables as Variables
-import icepyx.core.validate_inputs as val
-from icepyx.core.auth import EarthdataAuthMixin
 import icepyx.core.spatial as spat
 import icepyx.core.temporal as tp
+import icepyx.core.validate_inputs as val
+from icepyx.core.variables import Variables as Variables
 from icepyx.core.visualization import Visualize
 
 

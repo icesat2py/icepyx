82fafe75e7d9d1b07b3e33ace845c36bbe9c8f89
Jessica Scheick <JessicaS11@users.noreply.github.com>
2024-01-05 14:36:23 -0500

    Revert "traffic updates Aug-Dec 2023 (#477)"
    This reverts commit d2dbb492a695102139d2e25a5abe0fa3bb36c4c5.

--- doc/source/tracking/traffic/traffic_data_mgmt.py
+++ doc/source/tracking/traffic/traffic_data_mgmt.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import subprocess
 
 cwd = os.getcwd()
 
@@ -51,6 +52,5 @@ views.sort_values("_date").plot(
 
 fig.savefig(trafficpath + "plots.svg")
 
-# removing the files should not be necessary here
-# since they're not included in the git commit
+# removing the files should not be necessary here since they're not included in the git commit
 # subprocess.run(["rm -rf " + defaultpath[:-1]], shell=True)

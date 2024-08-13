f69b70e3256cbc47df6579366058c9cab9566ca6
Christian Clauss <cclauss@me.com>
2024-08-13 16:30:39 +0200

    print(os.environ)

--- icepyx/tests/test_auth.py
+++ icepyx/tests/test_auth.py
@@ -19,9 +19,10 @@ def auth_instance():
 
 # Test that .session creates a session
 @pytest.mark.skipif(
-    os.environ.get("TRAVIS") == "true", "Skipping this test on Travis CI."
+    os.getenv("TRAVIS"), "Skipping this test on Travis CI."
 )
 def test_get_session(auth_instance):
+    print(os.environ)
     assert isinstance(auth_instance.session, requests.sessions.Session)
 
 

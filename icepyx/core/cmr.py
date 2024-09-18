cf19a93805cda2ff36d217722d09c7152ccdb3e0
Matt Fisher <mfisher87@gmail.com>
2024-09-17 20:25:39 -0600

    Add concept_id cached property to Query class
    We need this to talk to Harmony -- Harmony doesn't support the product
    version as a query parameter, so we need to look up the real unique
    identifier in advance.

--- icepyx/core/cmr.py
+++ icepyx/core/cmr.py
@@ -1,4 +1,29 @@
 from typing import Final
 
+import requests
+
+from icepyx.core.urls import COLLECTION_SEARCH_BASE_URL
+from icepyx.uat import EDL_ACCESS_TOKEN
+
 CMR_PROVIDER: Final = "NSIDC_CUAT"
 # CMR_PROVIDER: Final = "NSIDC_CPRD"
+
+
+def get_concept_id(*, product: str, version: str) -> str:
+    response = requests.get(
+        COLLECTION_SEARCH_BASE_URL,
+        headers={
+            "Authorization": f"Bearer {EDL_ACCESS_TOKEN}",
+        },
+        params={
+            "short_name": product,
+            "version": version,
+            "provider": CMR_PROVIDER,
+        },
+    )
+    metadata = response.json()["feed"]["entry"]
+
+    if len(metadata) != 1:
+        raise RuntimeError(f"Expected 1 result from CMR, received {metadata}")
+
+    return metadata[0]["id"]

e7ff043d51c293b48d09c2ed685c12487fdc6bd4
Trey Stafford <trey.stafford@colorado.edu>
2024-11-11 14:57:31 -0700

    Harmony api: use `EarthdataAuthMixin`

--- icepyx/core/harmony.py
+++ icepyx/core/harmony.py
@@ -2,13 +2,22 @@ from typing import Any
 
 import harmony
 
+from icepyx.core.auth import EarthdataAuthMixin
 
-def get_capabilities(concept_id: str) -> dict[str, Any]:
-    capabilities_request = harmony.CapabilitiesRequest(concept_id=concept_id)
-    # TODO: This will work if the user has a .netrc file available but the other
-    # auth options might fail. We might need to add harmony client auth to the
-    # icepyx auth package.
-    harmony_client = harmony.Client()
-    response = harmony_client.submit(capabilities_request)
 
-    return response
+class HarmonyApi(EarthdataAuthMixin):
+    def __init__(self):
+        # initialize authentication properties
+        EarthdataAuthMixin.__init__(self)
+        self.harmony_client = harmony.Client(
+            auth=(
+                self.auth.username,
+                self.auth.password,
+            ),
+        )
+
+    def get_capabilities(self, concept_id: str) -> dict[str, Any]:
+        capabilities_request = harmony.CapabilitiesRequest(concept_id=concept_id)
+        response = self.harmony_client.submit(capabilities_request)
+
+        return response

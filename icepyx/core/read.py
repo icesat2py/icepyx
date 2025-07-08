ac093829c6bbc0502207ab4eeb91a4afbd0e684f
Jessica Scheick <JessicaS11@users.noreply.github.com>
2025-07-08 10:23:09 -0400

    add migrate to earthaccess goal note

--- icepyx/core/read.py
+++ icepyx/core/read.py
@@ -591,6 +591,8 @@ class Read(EarthdataAuthMixin):
                 # If path is an s3 path create an s3fs filesystem to reference the file
                 # TODO would it be better to be able to generate an s3fs session from the Mixin?
                 s3 = earthaccess.get_s3fs_session(daac="NSIDC")
+                # Goal: delegate most of the granule reading logic to earthaccess (xref https://github.com/icesat2py/icepyx/issues/575)
+                # See also: https://github.com/icesat2py/icepyx/pull/677/files#r2083622039
                 fsspec_params = {
                     "cache_type": "blockcache",
                     "block_size": 8 * 1024 * 1024,

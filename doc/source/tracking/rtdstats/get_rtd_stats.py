cda933e50643341456ace31797d122159fddd581
Jessica Scheick <jbscheick@gmail.com>
2026-06-01 15:37:14 -0400

    update python script given api doesn't provide analytics data

--- doc/source/tracking/rtdstats/get_rtd_stats.py
+++ doc/source/tracking/rtdstats/get_rtd_stats.py
@@ -14,6 +14,7 @@ headers = {"Authorization": f"Token {rtd_token}"}
 base = "https://app.readthedocs.org"
 
 
+# Turns out you cannot yet get the analytics data from the API, only the UI
 def fetch_rtd_analytics(url, headers, output_path=None):
     """Fetch analytics from a Read the Docs URL and return a DataFrame."""
     try:
@@ -48,37 +49,41 @@ def fetch_rtd_analytics(url, headers, output_path=None):
 
 
 ### WIP to collect the analytics data and combine it with whatever already exists
-pageviews = fetch_rtd_analytics(
-    f"{base}/api/v3/projects/icepyx/search-analytics/", headers
-)
+# pageviews = fetch_rtd_analytics(
+#     f"{base}/api/v3/projects/icepyx/", headers
+# )
+pageviews = pd.read_csv(trackpath + "pageview.csv")
 exist_pageviews = pd.read_csv(trackpath + pageviewfn)
 
 pageviews = pageviews.merge(
-    exist_pageviews, how="outer", on=["category", "date", "downloads"]
+    exist_pageviews, how="outer", on=["Path", "Date", "Version", "Views"]
 )
 
-pageviews.sort_values(["category", "date"], ignore_index=True).to_csv(
+# remove duplicate entries; sort default is ascending
+pageviews = pageviews.sort_values(["Date"], ignore_index=True).drop_duplicates(
+    subset=["Date", "Version", "Path"], keep="last"
+)
+
+pageviews.sort_values(["Date"], ignore_index=True).to_csv(
     trackpath + pageviewfn, index=False
 )
 
+# see which pages have most views
+pageviews.groupby("Path").sum().sort_values(["Views"], ascending=False)
 
-searches = fetch_rtd_analytics(f"{base}/api/v3/projects/icepyx/search-terms/", headers)
+
+# searches = fetch_rtd_analytics(f"{base}/api/v3/projects/icepyx/search-terms/", headers)
+
+searches = pd.read_csv(trackpath + "searches.csv")
 exist_searches = pd.read_csv(trackpath + searchfn)
 
 searches = searches.merge(
-    exist_searches, how="outer", on=["category", "date", "downloads"]
+    exist_searches, how="outer", on=["Created Date", "Query", "Total Results"]
 )
 
-searches.sort_values(["category", "date"], ignore_index=True).to_csv(
+searches.sort_values(["Query"], ignore_index=True).to_csv(
     trackpath + searchfn, index=False
 )
 
-
-### create some sort of summary info (e.g. cumulative/last 3 months most viewed page?)
-
-# dl_data = dl_data.groupby("category").get_group("without_mirrors").sort_values("date")
-
-# chart = dl_data.plot(
-#     x="date", y="downloads", figsize=(10, 2), label="Number of PyPI Downloads"
-# )
-# chart.figure.savefig(trackpath + "downloads.svg")
+# print out what most common query words are
+searches.groupby("Query").count()
